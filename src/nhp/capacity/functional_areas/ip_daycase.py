ceb1452947b937a13ea261126ed76a6027fa352d
YiWen Hon <yiwen.hon1@nhs.net>
2026-03-19 13:21:31 +0000

    add ip_daycase functions

---
+++ src/nhp/capacity/functional_areas/ip_daycase.py
@@ -0,0 +1,126 @@
+import pyspark.sql.functions as F
+import pandas as pd
+from pyspark.sql import DataFrame
+from nhp.capacity.functional_areas.processing_helpers import add_missing_groupings
+from databricks.connect import DatabricksSession
+from typing import List
+
+spark = DatabricksSession.builder.getOrCreate()
+
+
+def get_tretspef_lookup(
+    excel_url: str = "/Volumes/nhp/reference/files/0028452019codelistspecificationv1.2.xlsx",
+) -> DataFrame:
+    tretspef_lookup = pd.read_excel(
+        excel_url, engine="openpyxl", sheet_name="Treatment Function Codes"
+    )[["DD Code", "Group"]]
+    tretspef_lookup["DD Code"] = tretspef_lookup["DD Code"].astype(int)
+    tretspef_lookup_df = spark.createDataFrame(
+        tretspef_lookup.rename(
+            columns={"DD Code": "tretspef", "Group": "tretspef_type"}
+        )
+    )
+    return tretspef_lookup_df
+
+
+def add_tretspef_type(
+    ip_original: DataFrame, tretspef_lookup_df: DataFrame
+) -> DataFrame:
+    return ip_original.join(
+        tretspef_lookup_df,
+        on=ip_original["tretspef"] == tretspef_lookup_df["tretspef"],
+        how="left",
+    ).drop(tretspef_lookup_df["tretspef"])
+
+
+def create_ip_daycase_groupings(ip_data: DataFrame) -> DataFrame:
+    daycase_only = ip_data.filter(
+        F.col("admimeth").like("1%") & (F.col("classpat") == 2)
+    )
+    df_with_grouping = (
+        daycase_only.withColumn(
+            "grouping",
+            F.when(
+                (F.col("tretspef_type") == "Surgical Services")
+                & (F.col("admiage") > 17),
+                "adult_surgical_daycase",
+            )
+            .when(
+                (F.col("tretspef_type") == "Medical Services")
+                & (F.col("admiage") > 17),
+                "adult_medical_daycase",
+            )
+            .when(
+                (F.col("tretspef_type") == "Surgical Services")
+                & (F.col("admiage") <= 17),
+                "paediatric_surgical_daycase",
+            )
+            .when(
+                (F.col("tretspef_type") == "Medical Services")
+                & (F.col("admiage") <= 17),
+                "paediatric_medical_daycase",
+            )
+            .when((F.col("admiage") > 17), "adult_unknown_daycase")
+            .when((F.col("admiage") <= 17), "paediatric_unknown_daycase"),
+        )
+        .groupby("grouping", "model_run")
+        .agg(F.count("rn").alias("total"))
+    )
+    return df_with_grouping
+
+
+def process_ip_daycase(
+    ip_original_mapped: DataFrame, ip_model_results: DataFrame
+) -> DataFrame:
+    baseline_grouped = create_ip_daycase_groupings(ip_original_mapped)
+    groupings_per_run = create_ip_daycase_groupings(
+        ip_original_mapped.drop("speldur", "classpat", "model_run").join(
+            ip_model_results, on="rn", how="left"
+        )
+    )
+    final_groupings_per_run_with_baseline = groupings_per_run.unionByName(
+        baseline_grouped
+    )
+    # Add missing groupings - we need all groupings to be present in all model runs even if value is 0
+    required_ip_groupings = [
+        "adult_surgical_daycase",
+        "adult_medical_daycase",
+        "paediatric_surgical_daycase",
+        "paediatric_medical_daycase",
+        "adult_unknown_daycase",
+        "paediatric_unknown_daycase",
+    ]
+    final_df = add_missing_groupings(
+        final_groupings_per_run_with_baseline, required_ip_groupings
+    )
+    return final_df
+
+
+def qa_ip_daycase_results(
+    default_results: DataFrame, final_ip_daycase_df: DataFrame, sites: List[str]
+):
+    if "ALL" not in sites:
+        default_results = default_results.where((F.col("sitetret").isin(sites)))
+    default_results = default_results.where(
+        (F.col("pod").like("%daycase%")) & (F.col("measure") == "admissions")
+    )
+    default_results_value = (
+        default_results.groupBy("model_run")
+        .agg(F.sum("value").alias("value"))
+        .agg(F.mean("value").alias("mean"))
+        .collect()[0][0]
+    )
+    grouped_results_value = (
+        final_ip_daycase_df.filter(F.col("model_run") != 0)  # model_run 0 is baseline
+        .groupBy("model_run")
+        .agg(F.sum("total").alias("total"))
+        .agg(F.mean("total").alias("mean_total"))
+        .collect()[0][0]
+    )
+    try:
+        assert float(default_results_value) == float(grouped_results_value)
+    except AssertionError:
+        print(
+            "Aggregated results are not aligned with default model results. Check daycase calculations"
+        )
+        raise
